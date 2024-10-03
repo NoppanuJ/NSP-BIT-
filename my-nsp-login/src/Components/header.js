3920b8d0b14702f848d881a5fb04ee6167afabfb
NoppanuJ <64070276@kmitl.ac.th>
2024-10-03 22:04:37 +0700

    Update header.js

--- my-nsp-login/src/Components/header.js
+++ my-nsp-login/src/Components/header.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import '../CssComponents/Dashboard.css';
-import { FaHome, FaCalendarAlt, FaClipboardList, FaBell, FaUserCircle, FaCaretDown, FaThLarge, FaUser, FaCog, FaSignOutAlt, FaSun, FaMoon, FaUserNurse } from 'react-icons/fa';
+import { FaBell, FaUserCircle, FaUserNurse, FaUser, FaCog, FaSignOutAlt, FaSun, FaMoon } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import dayjs from 'dayjs';
 
 const Header = ({ setBar, bar, role }) => {
-    const [profileDropdown, setProfileDropdown] = useState(false); // State for dropdown visibility
-    const [darkMode, setDarkMode] = useState(false); // State for theme
-    const [currentTime, setCurrentTime] = useState(dayjs()); // Set initial state for time
+    const [profileDropdown, setProfileDropdown] = useState(false); // Profile dropdown state
+    const [nurseDropdown, setNurseDropdown] = useState(false); // Nurse dropdown state
+    const [darkMode, setDarkMode] = useState(false); // Dark mode state
+    const [currentTime, setCurrentTime] = useState(dayjs()); // Current time
 
     const navigate = useNavigate();
 
@@ -20,15 +21,18 @@ const Header = ({ setBar, bar, role }) => {
         navigate('/profiledisplay');
     };
     const settings = () => {
-        navigate('/setting'); // Navigate to settings page
+        navigate('/setting');
     };
     const logout = () => {
-        // Add your logout logic here, for now, we just navigate to login
         navigate('/');
     };
 
     const toggleProfileDropdown = () => {
         setProfileDropdown(!profileDropdown);
+    };
+
+    const toggleNurseDropdown = () => {
+        setNurseDropdown(!nurseDropdown);
     };
 
     const toggleBar = () => {
@@ -48,72 +52,28 @@ const Header = ({ setBar, bar, role }) => {
     }, []);
 
     return (
-
         <>
-            {role === "user" ? (<AppBar position="static" sx={{ backgroundColor: '#e0e0e0' }}>
-                <Toolbar >
-                    <IconButton edge="start" color="black" aria-label="menu" sx={{ mr: 2 }} onClick={toggleBar}>
-
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
-                        NSP
-                    </Typography>
-
-                    <FaBell onClick={notification} className="header-icon" color='black' />
-                    <div className="profile-container">
-                        <FaUserCircle onClick={toggleProfileDropdown} className="header-icon" color='black' />
-                        {profileDropdown && (
-                            <div className="profile-dropdown">
-                                <div onClick={ProfileDisplay} className="dropdown-item" style={{ color: 'black' }}>
-                                    <FaUser style={{ marginRight: '10px' }} /> Profile
-                                </div>
-                                <div onClick={settings} className="dropdown-item" style={{ color: 'black' }} >
-                                    <FaCog style={{ marginRight: '10px' }} /> Setting
-                                </div>
-                                <div onClick={toggleTheme} className="dropdown-item" style={{ color: 'black' }} >
-                                    {darkMode ? (
-                                        <>
-                                            <FaSun style={{ marginRight: '10px' }} /> Light Mode
-                                        </>
-                                    ) : (
-                                        <>
-                                            <FaMoon style={{ marginRight: '10px' }} /> Dark Mode
-                                        </>
-                                    )}
-                                </div>
-                                <div onClick={logout} className="dropdown-item" style={{ color: 'black' }}>
-                                    <FaSignOutAlt style={{ marginRight: '10px' }} /> Log Out
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </Toolbar>
-            </AppBar>) : (
-
+            {role === "user" ? (
                 <AppBar position="static" sx={{ backgroundColor: '#e0e0e0' }}>
-                    <Toolbar >
+                    <Toolbar>
                         <IconButton edge="start" color="black" aria-label="menu" sx={{ mr: 2 }} onClick={toggleBar}>
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
-                            NSP System V.1
+                            NSP
                         </Typography>
-                        <Typography variant="h6" sx={{ marginRight: 2, color: 'black' }}>
-                            {currentTime.format('HH:mm:ss')}
-                        </Typography>
-                        <FaUserNurse  className="header-icon" color='black' />
-                        {/* <div className="profile-container">
+                        <FaBell onClick={notification} className="header-icon" color='black' />
+                        <div className="profile-container">
                             <FaUserCircle onClick={toggleProfileDropdown} className="header-icon" color='black' />
                             {profileDropdown && (
                                 <div className="profile-dropdown">
                                     <div onClick={ProfileDisplay} className="dropdown-item" style={{ color: 'black' }}>
                                         <FaUser style={{ marginRight: '10px' }} /> Profile
                                     </div>
-                                    <div onClick={settings} className="dropdown-item" style={{ color: 'black' }} >
+                                    <div onClick={settings} className="dropdown-item" style={{ color: 'black' }}>
                                         <FaCog style={{ marginRight: '10px' }} /> Setting
                                     </div>
-                                    <div onClick={toggleTheme} className="dropdown-item" style={{ color: 'black' }} >
+                                    <div onClick={toggleTheme} className="dropdown-item" style={{ color: 'black' }}>
                                         {darkMode ? (
                                             <>
                                                 <FaSun style={{ marginRight: '10px' }} /> Light Mode
@@ -129,11 +89,34 @@ const Header = ({ setBar, bar, role }) => {
                                     </div>
                                 </div>
                             )}
-                        </div> */}
+                        </div>
                     </Toolbar>
                 </AppBar>
-            )
-            }
+            ) : (
+                <AppBar position="static" sx={{ backgroundColor: '#e0e0e0' }}>
+                    <Toolbar>
+                        <IconButton edge="start" color="black" aria-label="menu" sx={{ mr: 2 }} onClick={toggleBar}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
+                            NSP System V.1
+                        </Typography>
+                        <Typography variant="h6" sx={{ marginRight: 2, color: 'black' }}>
+                            {currentTime.format('HH:mm:ss')}
+                        </Typography>
+                        <div className="profile-container">
+                            <FaUserNurse onClick={toggleNurseDropdown} className="header-icon" color='black' />
+                            {nurseDropdown && (
+                                <div className="profile-dropdown">
+                                    <div onClick={logout} className="dropdown-item" style={{ color: 'black' }}>
+                                        <FaSignOutAlt style={{ marginRight: '10px' }} /> Log Out
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            )}
         </>
     );
 };
