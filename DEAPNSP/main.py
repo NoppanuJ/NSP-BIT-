819d7cb4ed0c22826d39635661dfc709b836677c
Tangpanitharn <64070273@kmitl.ac.th>
2024-11-11 11:11:54 +0700

    Update main.py

--- DEAPNSP/main.py
+++ DEAPNSP/main.py
@@ -1,107 +1,61 @@
-from deap import base
-from deap import creator
 from deap import tools
+from deap import algorithms
 
-import random
-import numpy
+def eaSimpleWithElitism(population, toolbox, cxpb, mutpb, ngen, stats=None,
+             halloffame=None, verbose=__debug__):
+    """This algorithm is similar to DEAP eaSimple() algorithm, with the modification that
+    halloffame is used to implement an elitism mechanism. The individuals contained in the
+    halloffame are directly injected into the next generation and are not subject to the
+    genetic operators of selection, crossover and mutation.
+    """
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
 
-import matplotlib
-print(matplotlib.__version__)
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+    if halloffame is None:
+        raise ValueError("halloffame parameter must not be empty!")
 
-from NurseSchedulingProblem import  NurseSchedulingProblem
-from eaSimpleWithElitism import eaSimpleWithElitism
+    halloffame.update(population)
+    hof_size = len(halloffame.items) if halloffame.items else 0
 
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print(logbook.stream)
 
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
 
-# problem constants:
-HARD_CONSTRAINT_PENALTY = 10000  # the penalty factor for a hard-constraint violation
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population) - hof_size)
 
-# Genetic Algorithm constants:
-POPULATION_SIZE = 300
-P_CROSSOVER = 0.9  # probability for crossover
-P_MUTATION = 0.1   # probability for mutating an individual
-MAX_GENERATIONS = 200
-HALL_OF_FAME_SIZE = 30
+        # Vary the pool of individuals
+        offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
 
-# set the random seed:
-RANDOM_SEED = 42
-random.seed(RANDOM_SEED)
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
 
-toolbox = base.Toolbox()
+        # add the best back to population:
+        offspring.extend(halloffame.items)
 
-# create the nurse scheduling problem instance to be used:
-nsp = NurseSchedulingProblem(HARD_CONSTRAINT_PENALTY)
+        # Update the hall of fame with the generated individuals
+        halloffame.update(offspring)
 
-# define a single objective, maximizing fitness strategy:
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        # Replace the current population by the offspring
+        population[:] = offspring
 
-# create the Individual class based on list:
-creator.create("Individual", list, fitness=creator.FitnessMin)
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            print(logbook.stream)
 
-# create an operator that randomly returns 0 or 1:
-toolbox.register("zeroOrOne", random.randint, 0, 1)
-
-# create the individual operator to fill up an Individual instance:
-toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.zeroOrOne, len(nsp))
-
-# create the population operator to generate a list of individuals:
-toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
-
-
-# fitness calculation
-def getCost(individual):
-    return nsp.getCost(individual),  # return a tuple
-
-
-toolbox.register("evaluate", getCost)
-
-# genetic operators:
-toolbox.register("select", tools.selTournament, tournsize=2)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=1.0/len(nsp))
-
-
-# Genetic Algorithm flow:
-def main():
-
-    # create initial population (generation 0):
-    population = toolbox.populationCreator(n=POPULATION_SIZE)
-
-    # prepare the statistics object:
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("min", numpy.min)
-    stats.register("avg", numpy.mean)
-
-    # define the hall-of-fame object:
-    hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
-
-    # perform the Genetic Algorithm flow with hof feature added:
-    population, logbook = eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
-                                              ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
-
-    # print best solution found:
-    best = hof.items[0]
-    print("-- Best Individual = ", best)
-    print("-- Best Fitness = ", best.fitness.values[0])
-    print()
-    print("-- Schedule = ")
-    nsp.printScheduleInfo(best)
-
-    # extract statistics:
-    minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
-
-    # plot statistics:
-    sns.set_style("whitegrid")
-    plt.plot(minFitnessValues, color='red')
-    plt.plot(meanFitnessValues, color='green')
-    plt.xlabel('Generation')
-    plt.ylabel('Min / Average Fitness')
-    plt.title('Min and Average fitness over Generations')
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
+    return population, logbook
