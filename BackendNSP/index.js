4f374f64247660e94f55716df861bb33173c0556
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2024-11-29 17:18:41 +0700

    fix : bug block localhost:300

--- BackendNSP/index.js
+++ BackendNSP/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 // Load environment variables
 dotenv.config();
 
 // Initialize app
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000', // ระบุ origin ที่อนุญาต
+  methods: ['GET', 'POST'], // ระบุ HTTP methods ที่อนุญาต
+  credentials: true, // ถ้าจำเป็นต้องใช้ cookies หรือ authorization headers
+}));
+// Middleware
+app.use(express.json()); // สำหรับ JSON payload
+app.use(express.urlencoded({ extended: true })); // สำหรับ URL-encoded payload
+
 
 // Connect to database
 connectDB();
@@ -15,8 +25,8 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/nurses', require('./routes/nurse.route'));
-
+app.use('/', require('./routes/nurse.route'));
+app.use('/', require('./routes/login.route'));
 // Basic route
 // app.get('/', (req, res) => {
 //   res.send('Welcome to the API');
